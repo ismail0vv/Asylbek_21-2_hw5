d46107ec383f3d7ecf57b00c7e711bea8b22a718
ismail0vv <110819132+ismail0vv@users.noreply.github.com>
2022-08-20 00:14:14 +0600

    Update lottery.py

--- lottery.py
+++ lottery.py
@@ -3,7 +3,7 @@ lst = [i for i in range(1, 31)]
 def stavka(lot, amount):
     win_lot = choice(lst)
     if lot == win_lot:
-        print(f'Вы выиграли {amount*2}$ :(')
+        print(f'Вы выиграли {amount*2}$ T_T')
         return amount * 2
     print(f'Вы проиграли {amount}$ :D')
     return -amount
